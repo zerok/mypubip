7deaa68424174fec603d8b98fa79dd5000ba5fd9
Horst Gutmann <zerok@zerokspot.com>
2022-03-06 18:21:33 +0100

    Add optional OpenTelemetry support

--- go.mod
+++ go.mod
@@ -5,5 +5,10 @@ go 1.15
 require (
 	github.com/rs/zerolog v1.20.0
 	github.com/spf13/pflag v1.0.5
-	github.com/stretchr/testify v1.6.1
+	github.com/stretchr/testify v1.7.0
+	go.opentelemetry.io/otel v1.4.1
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.4.1 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.4.1 // indirect
+	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.4.1
+	go.opentelemetry.io/otel/sdk v1.4.1
 )
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net"
 	"net/http"
@@ -8,9 +9,20 @@ import (
 
 	"github.com/rs/zerolog"
 	"github.com/spf13/pflag"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.7.0"
 )
 
 func echoIPHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	tracer := otel.Tracer(tracerName)
+	_, span := tracer.Start(ctx, "echoIPHandler")
+	defer span.End()
 	remoteIP := ""
 	remoteIPs := r.Header.Values("X-Forwarded-For")
 	if len(remoteIPs) > 0 {
@@ -19,6 +31,8 @@ func echoIPHandler(w http.ResponseWriter, r *http.Request) {
 	if remoteIP == "" {
 		host, _, err := net.SplitHostPort(r.RemoteAddr)
 		if err != nil {
+			span.RecordError(err)
+			span.SetStatus(codes.Error, "hostname splittint failed")
 			http.Error(w, "Unexpected remote address", http.StatusBadRequest)
 			return
 		}
@@ -26,21 +40,49 @@ func echoIPHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	ip := net.ParseIP(remoteIP)
 	if ip == nil {
+		span.RecordError(fmt.Errorf("invalid IP address"))
+		span.SetStatus(codes.Error, "IP could not be parsed")
 		http.Error(w, "Invalid IP", http.StatusBadRequest)
 		return
 	}
 	fmt.Fprint(w, ip.String())
 }
 
+const tracerName = "mypubip"
+
 func main() {
 	logger := zerolog.New(zerolog.ConsoleWriter{Out: os.Stdout}).With().Timestamp().Logger()
 	var addr string
+	var otlpTracing bool
 	pflag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "Usage of mypubip:\n")
 		pflag.PrintDefaults()
 	}
 	pflag.StringVar(&addr, "addr", "localhost:8000", "Address to listen on")
+	pflag.BoolVar(&otlpTracing, "telemetry", false, "Enable telemetry submission via OTLP")
 	pflag.Parse()
+
+	if otlpTracing {
+		logger.Info().Msg("Telemetry enabled")
+		client := otlptracehttp.NewClient(otlptracehttp.WithInsecure())
+		exporter, err := otlptrace.New(context.Background(), client)
+		if err != nil {
+			logger.Fatal().Err(err).Msg("Failed to create tracing exporter")
+		}
+		res, err := resource.Merge(
+			resource.Default(),
+			resource.NewWithAttributes(
+				semconv.SchemaURL,
+				semconv.ServiceNameKey.String("mypubip"),
+			),
+		)
+		if err != nil {
+			logger.Fatal().Err(err).Msg("Failed to build telemetry resource")
+		}
+		tp := trace.NewTracerProvider(trace.WithBatcher(exporter), trace.WithResource(res))
+		otel.SetTracerProvider(tp)
+	}
+
 	srv := http.Server{}
 	srv.Addr = addr
 	srv.Handler = http.HandlerFunc(echoIPHandler)
